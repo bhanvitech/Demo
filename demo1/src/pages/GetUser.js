19071f972d42ac930e316feef7499c17aa2ca8f4
bhanvi <bhanvi@gaia.in>
2025-01-22 17:53:59 +0530

    gg

--- demo1/src/pages/GetUser.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GetUser = () => {
-  return <div>GetUser</div>;
-};
-
-export default GetUser;
--- demo1/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axiosInstance from "./axiosConfig";
-
-export const api = {
-  get: (url, params = {}, config = {}) =>
-    axiosInstance.get(url, { params, ...config }),
-  post: (url, data = {}, config = {}) => axiosInstance.post(url, data, config),
-  put: (url, data = {}, config = {}) => axiosInstance.put(url, data, config),
-  delete: (url, config = {}) => axiosInstance.delete(url, config),
-};
